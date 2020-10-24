16f827387e4160c63e45cc971a37c430b3e1fe55
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-10-24 12:04:41 -0300

    Fix node_modules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,10 @@
 import React from "react";
+import "./app.styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Platzi Frontend Developer Test</h1>
     </div>
   );
 }
