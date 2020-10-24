bb7aea0939fc52ad1e18f5d27d0392e8fb34d063
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-10-24 12:06:46 -0300

    Fix node modules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./app.styles.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Platzi Frontend Developer Test</h1>
     </div>
   );
-}
+};
 
 export default App;
